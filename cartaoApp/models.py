f8e4a46fd23603b0924799d3d443d3f5c658c78e
aluno <rogerio.pereira@ifto.edu.br>
2024-11-29 08:32:36 -0300

    versão 1.0

---
+++ cartaoApp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Validar(models.Model):
+    id_validar = models.AutoField(primary_key=True)
+    cartao_validar =models.CharField(max_length=255 ,null=False)
+    cpf_validar =models.CharField(max_length=255 ,null=False)
+    nome_validar =models.CharField(max_length=255 ,null=False)
+    codigo_validar =models.CharField(max_length=255 ,null=False)
+    data_validar =models.CharField(max_length=255 ,null=False)
+
+    def __str__(self):
+        return self.nome_validar
